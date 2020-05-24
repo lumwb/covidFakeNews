2e9acdb17c9bf9f0998aea71c538402db8d8dae8
weiboon <lumweiboon@gmail.com>
2020-05-25 01:14:09 +0800

    Add host and port config

--- start_backend.py
+++ start_backend.py
@@ -323,6 +323,5 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get('PORT', 8888))
-    app.run(port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
